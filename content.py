078c5afe8e118122adac0f37ab09ddc1dfc7c283
pepesayshi <sean.li.lxy@gmail.com>
2022-05-31 20:45:32 +1200

    add csv files

--- content.py
+++ content.py
@@ -1,7 +1,5 @@
-import matplotlib
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 from pasta.augment import inline
 from sklearn.feature_extraction.text import TfidfVectorizer
